0d2547d236e9fd88f1c9ace69ef07db1e418d815
yoii18 <dpssalhotra@gmail.com>
2023-08-20 04:13:32 +0530

    hyperparams tuning

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import os
 import pickle
 import sys
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -20,12 +21,20 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
 
     try:
         report = {}
 
         for keys, model in zip(models.keys(), models.values()):
+
+            para=param[keys]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+
             model.fit(X_train, y_train)
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
